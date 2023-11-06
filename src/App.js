3cc33a353e0e3fc216c2cf1f233c0dcdd781b6ac
Anastasiia Tabakova <atabakova2016@gmail.com>
2023-11-06 19:04:40 +0000

    commit description

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<Project />} />
           <Route path="/contacts" element={<Contacts />} />
