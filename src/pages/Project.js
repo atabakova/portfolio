5596929aa06bc09f5feda5af369feef989172b00
Anastasiya Tabakova <atabakova2016@gmail.com>
2023-11-28 13:13:35 +0000

    new projects

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -9,7 +9,7 @@ function Project() {
   //const project = projects[id];
 
   let project = projects[id];
-  if (id >= '8') project = petprojects[id];
+  if (id >= 8) project = petprojects[id];
 
   console.log(petprojects);
   return (
